caf63f0ab12ad81abb533eecb6b14d6ea5d97ce2
MaykonGomes <maykon.gomes25@gmail.com>
2021-11-17 08:01:19 -0300

    primeiro git

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_script._compat import text_type
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+from app.controllers import default
+
+
